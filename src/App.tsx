6e49181901ebe1b6db030c0936e2ae44c2289128
Fred011 <frederic.vannier01@gmail.com>
2021-03-17 10:07:07 +0100

    wpm fixed by adding another array for the function wordsPerMinute

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import WordsList from "./components/WordsList"
 interface State {
   typeTest: string;
   words: Array<string>;
+  updatedWords: Array<string>;
   enteredText: string;
   score: number;
   started: boolean;
@@ -17,6 +18,7 @@ class App extends Component {
   state: State = {
     typeTest: " ",
     words: [],
+    updatedWords: [],
     enteredText: "",
     score: 0,
     started: false,
@@ -25,20 +27,22 @@ class App extends Component {
   };
 
   async componentDidMount() {
-    const url = "https://random-word-api.herokuapp.com//word?number=3";
+    const url = "https://random-word-api.herokuapp.com//word?number=10";
     const response = await fetch(url);
     const data = await response.json();
-    this.setState({ words: data});
+    this.setState({ words: data });
+    this.setState({ updatedWords: data });
   }
 
   wordsPerMinute = (charsTyped: number, millis: number): number =>
     Math.floor(charsTyped / 5 / (millis / 60000));
 
   checkFinished = (): void => {
-    if (!this.state.words.length) {
+    if (!this.state.updatedWords.length) {
       if (this.state.startTime) {
         const timeMillis: number =
           new Date().getTime() - this.state.startTime.getTime();
+          console.log("timeMillis", timeMillis);
         const wpm = this.wordsPerMinute(this.state.words.length, timeMillis);
         console.log("WPMMMMM", wpm);
         this.setState({ wordsPerMinute: wpm }, (): void => console.log('wpm', this.state.wordsPerMinute));
@@ -50,20 +54,16 @@ class App extends Component {
     if (!this.state.started) {
       this.setState({ started: true, startTime: new Date() });
     }
-    console.log(e.currentTarget.value);
-    //console.log('start time', this.state.startTime);
+
     let enteredText = e.currentTarget.value;
-    this.setState({ enteredText });
-    console.log(this.state.startTime);
-    console.log(this.state.started);
+    this.setState({ enteredText }, (): void => console.log("ENTERED TEXT", this.state.enteredText));
     
-    if (enteredText === this.state.words[0] + ' ') {
+    if (enteredText === this.state.updatedWords[0] + ' ') {
       this.setState({ score: this.state.score + 1 });
-      this.setState({ words: this.state.words.slice(1) }, (): void => console.log('updated words', this.state.words)
+      this.setState({ updatedWords: this.state.updatedWords.slice(1) }, () => console.log('updated words', this.state.updatedWords)
       );
       this.setState({ enteredText: "" }, (): void => this.checkFinished());
     }
-    //this.checkFinished();
   };
 
   render() {
@@ -80,7 +80,7 @@ class App extends Component {
         <div className="container">
           <h3>Type the following</h3>
           <div className="quote-display" id="quote-display">
-            <h6>{this.state.words.map(word => word === this.state.words[0] ? <em className="current-word">{word} </em> : word + " ")}</h6>
+            <h6>{this.state.updatedWords.map(word => word === this.state.updatedWords[0] ? <em className="current-word">{word} </em> : word + " ")}</h6>
           </div>
           <input
             id="quoteInput"
